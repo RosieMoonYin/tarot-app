4b28f6ddd4fcbefa060b41e6a3b6139bd9000d9b
Rosie MoonYin <rosie.victoria.burnett@gmail.com>
2024-08-09 14:17:05 +0200

    feat: implement react useState on button click. Card img rerenders

--- Frontend-tarot-app/src/App.tsx
+++ Frontend-tarot-app/src/App.tsx
@@ -1,16 +1,32 @@
+import { useState } from "react";
+import frontImage from "./assets/tarot-card-front.png";
+import backImage from "./assets/tarot-card-back.png";
 import "./App.css";
 
 function App() {
+  const [isFront, setIsFront] = useState(true);
+  const [buttonText, setButtonText] = useState("Card Front");
+
+  const handleClick = () => {
+    setIsFront(!isFront);
+    setButtonText(isFront ? "Card Front" : "Card Back");
+  };
+
   return (
     <>
       <div>
         <h1>Tarot Reader</h1>
       </div>
       <div className="card">
-        <button>Mock Card</button>
+        <img
+          src={isFront ? frontImage : backImage}
+          alt="tarot card"
+          className="card-image"
+        />
+        <button onClick={handleClick}>{buttonText}</button>
       </div>
       <div>
-        <p>Click on the card to begin</p>
+        <p>Click on the button to pick a card</p>
       </div>
     </>
   );

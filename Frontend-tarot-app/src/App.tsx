6912468f25b261adce1ad547669a210729c242ff
Rosie MoonYin <rosie.victoria.burnett@gmail.com>
2024-08-09 13:37:45 +0200

    feat: set up Vite React project remove unnecessary boilerplate code

---
+++ Frontend-tarot-app/src/App.tsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Tarot Reader</h1>
+      </div>
+      <div className="card">
+        <button>Mock Card</button>
+      </div>
+      <div>
+        <p>Click on the card to begin</p>
+      </div>
+    </>
+  );
+}
+
+export default App;

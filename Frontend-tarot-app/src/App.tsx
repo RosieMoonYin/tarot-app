7970063d9cbb1594e5155e970913c22aab390a5e
Rosie MoonYin <rosie.victoria.burnett@gmail.com>
2024-08-09 15:26:59 +0200

    feat: expand tarot card obj into an array of obj, 3 more card objects added

--- Frontend-tarot-app/src/App.tsx
+++ Frontend-tarot-app/src/App.tsx
@@ -1,16 +1,45 @@
 import { useState } from "react";
-import frontImg from "./assets/tarot-card-front.png";
-import backImg from "./assets/tarot-card-back.png";
 import "./App.css";
 
-const tarotCard = {
-  id: 1,
-  name: "The Fool",
-  meaning:
-    "The Fool represents new beginnings, having faith in the future, and being carefree.",
-  frontImg: frontImg,
-  backImg: backImg,
-};
+import loversFront from "./assets/lovers.png";
+import emperorFront from "./assets/emperor.png";
+import hierophantFront from "./assets/hierophant.png";
+import frontImg from "./assets/tarot-card-front.png";
+import backImg from "./assets/tarot-card-back.png";
+
+const tarotCards = [
+  {
+    id: 1,
+    name: "The Fool",
+    meaning:
+      "The Fool represents new beginnings, having faith in the future, and being carefree.",
+    frontImg: frontImg,
+    backImg: backImg,
+  },
+  {
+    id: 2,
+    name: "The Lovers",
+    meaning: "The Lovers represent love, union, relationships, and harmony.",
+    frontImg: loversFront,
+    backImg: backImg,
+  },
+  {
+    id: 3,
+    name: "The Emperor",
+    meaning:
+      "The Emperor symbolizes authority, structure, control, and fatherhood.",
+    frontImg: emperorFront,
+    backImg: backImg,
+  },
+  {
+    id: 4,
+    name: "The Hierophant",
+    meaning:
+      "The Hierophant signifies tradition, spirituality, and religious beliefs.",
+    frontImg: hierophantFront,
+    backImg: backImg,
+  },
+];
 
 function App() {
   const [isFront, setIsFront] = useState(true);
@@ -28,7 +57,7 @@ function App() {
       </div>
       <div className="card">
         <img
-          src={isFront ? tarotCard.frontImg : tarotCard.backImg}
+          src={isFront ? tarotCards.backImg : tarotCards.frontImg}
           alt="tarot card"
           className="card-image"
         />

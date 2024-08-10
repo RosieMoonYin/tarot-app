10b63a86d55889499e4a15ef72221fb34d2e1487
Rosie MoonYin <rosie.victoria.burnett@gmail.com>
2024-08-10 22:40:58 +0200

    feat: import and use Modal component in App

--- Frontend-tarot-app/src/App.tsx
+++ Frontend-tarot-app/src/App.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { TarotCardType } from "./types/tarotCardType";
 import { imageMap } from "./utils/imageMap";
 import Card from "./components/card.tsx";
+import Modal from "./components/modal.tsx";
 
 function App() {
   const [tarotCards, setTarotCards] = useState<TarotCardType[]>([]);
@@ -62,30 +63,7 @@ function App() {
           imageMap={imageMap}
           onClick={handleClick}
         />
-
-        {/* <div className="card">
-          <img
-            src={
-              isFront
-                ? imageMap[currentCard.id].backImg
-                : imageMap[currentCard.id].frontImg
-            }
-            alt="tarot card"
-            className="card-image"
-            onClick={handleClick}
-          />
-        </div> */}
-        <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
-          <div className="modal-box">
-            <h3 className="font-bold text-lg">{currentCard.name}</h3>
-            <p className="py-4">{currentCard.meaning}</p>
-            <div className="modal-action">
-              <form method="dialog">
-                <button className="btn">Close</button>
-              </form>
-            </div>
-          </div>
-        </dialog>
+        <Modal card={currentCard} />
       </section>
     </>
   );

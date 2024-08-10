58fa1e9104d8c0bda720d3980c1f032304c3c9e4
Rosie MoonYin <rosie.victoria.burnett@gmail.com>
2024-08-10 20:39:06 +0200

    feat: useEffect implemented and debugged, working IMG paths

--- Frontend-tarot-app/src/App.tsx
+++ Frontend-tarot-app/src/App.tsx
@@ -1,50 +1,62 @@
 import "./App.css";
-
 import loversFront from "./assets/lovers.png";
 import emperorFront from "./assets/emperor.png";
 import hierophantFront from "./assets/hierophant.png";
 import backImg from "./assets/tarot-card-back.png";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { TarotCardType } from "./types/tarotCardType";
 
-const tarotCards = [
-  {
-    id: 1,
-    name: "The Fool",
-    meaning:
-      "The Fool represents new beginnings, having faith in the future, and being carefree.",
+const imageMap: {
+  [key: string]: {
+    frontImg: string;
+    backImg: string;
+  };
+} = {
+  "1": {
     frontImg: loversFront,
     backImg: backImg,
   },
-  {
-    id: 2,
-    name: "The Lovers",
-    meaning: "The Lovers represent love, union, relationships, and harmony.",
+  "2": {
     frontImg: loversFront,
     backImg: backImg,
   },
-  {
-    id: 3,
-    name: "The Emperor",
-    meaning:
-      "The Emperor symbolizes authority, structure, control, and fatherhood.",
+  "3": {
     frontImg: emperorFront,
     backImg: backImg,
   },
-  {
-    id: 4,
-    name: "The Hierophant",
-    meaning:
-      "The Hierophant signifies tradition, spirituality, and religious beliefs.",
+  "4": {
     frontImg: hierophantFront,
     backImg: backImg,
   },
-];
+};
 
 function App() {
+  const [tarotCards, setTarotCards] = useState<TarotCardType[]>([]);
+
   const [isFront, setIsFront] = useState(true);
   const [currentCard, setCurrentCard] = useState(tarotCards[0]);
 
-  // (document.getElementById("my_modal_5") as HTMLDialogElement).showModal();
+  useEffect(() => {
+    console.log("DEBUG useEffect is running");
+    fetch("http://localhost:5070/api/TarotCard")
+      .then((response) => {
+        console.log("DEBUG Fetch response YAY:", response);
+
+        if (!response.ok) {
+          throw new Error("sorry 200 OK was not returned");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log("DEBUG Fetched data YAY:", data);
+        setTarotCards(data);
+        setCurrentCard(data[0]);
+      })
+      .catch((error) => {
+        console.error("DEBUG the fetch didnt work here is the error:", error);
+      });
+  }, []);
+
   const showModal = (id: string) => {
     const modal = document.getElementById(id) as HTMLDialogElement;
     if (modal) {
@@ -61,6 +73,9 @@ function App() {
       showModal("my_modal_5");
     }
   };
+  if (!currentCard) {
+    return <p>I AM LOADING or empty...</p>;
+  }
   return (
     <>
       <div>
@@ -69,13 +84,16 @@ function App() {
       <section className="">
         <div className="card">
           <img
-            src={isFront ? currentCard.backImg : currentCard.frontImg}
+            src={
+              isFront
+                ? imageMap[currentCard.id].backImg
+                : imageMap[currentCard.id].frontImg
+            }
             alt="tarot card"
             className="card-image"
             onClick={handleClick}
           />
         </div>
-
         <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
           <div className="modal-box">
             <h3 className="font-bold text-lg">{currentCard.name}</h3>
---
+++ Frontend-tarot-app/src/types/tarotCardType.tsx
@@ -0,0 +1,5 @@
+export type TarotCardType = {
+  id: number;
+  name: string;
+  meaning: string;
+};
